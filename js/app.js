aee48c7da29fbfafd558aca287cb38d26ac6f66f
meshnesh <tonnie.nesh@gmail.com>
2016-10-26 17:56:15 +0300

    added the js code to hide and show divs

--- js/app.js
+++ js/app.js
@@ -1,1 +1,129 @@
-$(document).foundation()
+$('#promoti, #toddy').click(function () {
+	$('#promotion').css('display', 'block');
+	$('#vegetableProducts').css('display', 'none');
+	$('#fruitProducts').css('display', 'none');
+	$('#soapProducts').css('display', 'none');
+	$('#clothProducts').css('display', 'none');
+	$('#detergentProducts').css('display', 'none');
+	$('#perfumesProducts').css('display', 'none');
+	$('#lotionProducts').css('display', 'none');
+	$('#facialProducts').css('display', 'none');
+	$('#meatProducts').css('display', 'none');
+});
+
+$('#meaty').click(function () {
+	$('#meatProducts').css('display', 'block');
+	$('#promotion').css('display', 'none');
+	$('#vegetableProducts').css('display', 'none');
+	$('#fruitProducts').css('display', 'none');
+	$('#soapProducts').css('display', 'none');
+	$('#clothProducts').css('display', 'none');
+	$('#detergentProducts').css('display', 'none');
+	$('#perfumesProducts').css('display', 'none');
+	$('#lotionProducts').css('display', 'none');
+	$('#facialProducts').css('display', 'none');
+});
+
+$('#vegy').click(function () {
+	$('#vegetableProducts').css('display', 'block');
+	$('#promotion').css('display', 'none');
+	$('#meatProducts').css('display', 'none');
+	$('#fruitProducts').css('display', 'none');
+	$('#soapProducts').css('display', 'none');
+	$('#clothProducts').css('display', 'none');
+	$('#detergentProducts').css('display', 'none');
+	$('#perfumesProducts').css('display', 'none');
+	$('#lotionProducts').css('display', 'none');
+	$('#facialProducts').css('display', 'none');
+});
+
+$('#fruity').click(function () {
+	$('#fruitProducts').css('display', 'block');
+	$('#promotion').css('display', 'none');
+	$('#vegetableProducts').css('display', 'none');
+	$('#meatProducts').css('display', 'none');
+	$('#soapProducts').css('display', 'none');
+	$('#clothProducts').css('display', 'none');
+	$('#detergentProducts').css('display', 'none');
+	$('#perfumesProducts').css('display', 'none');
+	$('#lotionProducts').css('display', 'none');
+	$('#facialProducts').css('display', 'none');
+});
+
+$('#soapy').click(function () {
+	$('#soapProducts').css('display', 'block');
+	$('#promotion').css('display', 'none');
+	$('#vegetableProducts').css('display', 'none');
+	$('#fruitProducts').css('display', 'none');
+	$('#meatProducts').css('display', 'none');
+	$('#clothProducts').css('display', 'none');
+	$('#detergentProducts').css('display', 'none');
+	$('#perfumesProducts').css('display', 'none');
+	$('#lotionProducts').css('display', 'none');
+	$('#facialProducts').css('display', 'none');
+});
+
+$('#clothy').click(function () {
+	$('#clothProducts').css('display', 'block');
+	$('#promotion').css('display', 'none');
+	$('#vegetableProducts').css('display', 'none');
+	$('#fruitProducts').css('display', 'none');
+	$('#meatProducts').css('display', 'none');
+	$('#soapProducts').css('display', 'none');
+	$('#detergentProducts').css('display', 'none');
+	$('#perfumesProducts').css('display', 'none');
+	$('#lotionProducts').css('display', 'none');
+	$('#facialProducts').css('display', 'none');
+});
+
+$('#detery').click(function () {
+	$('#detergentProducts').css('display', 'block');
+	$('#promotion').css('display', 'none');
+	$('#vegetableProducts').css('display', 'none');
+	$('#fruitProducts').css('display', 'none');
+	$('#meatProducts').css('display', 'none');
+	$('#clothProducts').css('display', 'none');
+	$('#soapProducts').css('display', 'none');
+	$('#perfumesProducts').css('display', 'none');
+	$('#lotionProducts').css('display', 'none');
+	$('#facialProducts').css('display', 'none');
+});
+
+$('#perfy').click(function () {
+	$('#perfumesProducts').css('display', 'block');
+	$('#promotion').css('display', 'none');
+	$('#vegetableProducts').css('display', 'none');
+	$('#fruitProducts').css('display', 'none');
+	$('#meatProducts').css('display', 'none');
+	$('#clothProducts').css('display', 'none');
+	$('#soapProducts').css('display', 'none');
+	$('#detergentProducts').css('display', 'none');
+	$('#lotionProducts').css('display', 'none');
+	$('#facialProducts').css('display', 'none');
+});
+
+$('#oily').click(function () {
+	$('#lotionProducts').css('display', 'block');
+	$('#promotion').css('display', 'none');
+	$('#vegetableProducts').css('display', 'none');
+	$('#fruitProducts').css('display', 'none');
+	$('#meatProducts').css('display', 'none');
+	$('#clothProducts').css('display', 'none');
+	$('#soapProducts').css('display', 'none');
+	$('#perfumesProducts').css('display', 'none');
+	$('#detergentProducts').css('display', 'none');
+	$('#facialProducts').css('display', 'none');
+});
+
+$('#facy').click(function () {
+	$('#facy').css('display', 'block');
+	$('#promotion').css('display', 'none');
+	$('#vegetableProducts').css('display', 'none');
+	$('#fruitProducts').css('display', 'none');
+	$('#meatProducts').css('display', 'none');
+	$('#clothProducts').css('display', 'none');
+	$('#soapProducts').css('display', 'none');
+	$('#perfumesProducts').css('display', 'none');
+	$('#lotionProducts').css('display', 'none');
+	$('#detergentProducts').css('display', 'none');
+});
